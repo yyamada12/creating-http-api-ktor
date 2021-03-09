1db58cb32d3fecc5afbda9eeeba1cdbb0292660e
yyamada12 <12yacropolisy@gmail.com>
2021-03-10 07:13:16 +0900

    add customer endpoint

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,10 +1,16 @@
 package com.jetbrains.handson.httpapi
 
-import io.ktor.application.Application
+import com.jetbrains.handson.httpapi.routes.registerCustomerRoutes
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.serialization.*
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-
+    install(ContentNegotiation) {
+        json()
+    }
+    registerCustomerRoutes()
 }
